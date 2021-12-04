914822dfa3ae8d985a5bcb9815a7c2acf8bcd52d
Matheus Alves Pereira <67547806+matheusgit1@users.noreply.github.com>
2021-12-04 08:32:58 -0300

    commit

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,13 +9,13 @@ import "firebase/database"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBhHo25BRyv2ft_UGCxdJLWwpB2TEtMvVE",
-  authDomain: "heygrupos-24a3c.firebaseapp.com",
-  projectId: "heygrupos-24a3c",
-  storageBucket: "heygrupos-24a3c.appspot.com",
-  messagingSenderId: "443828787699",
-  appId: "1:443828787699:web:13263f2af9dea56116da86",
-  measurementId: "G-GV5QC4T80K"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
